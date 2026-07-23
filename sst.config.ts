17c11879ccdfe9c78a7dc1b3e4b76d2eed63cfbc
Malte Boeing <hello@malte.nz>
2026-07-23 15:24:59 +1200

    Ignore upload videos

--- sst.config.ts
+++ sst.config.ts
@@ -13,8 +13,35 @@ export default $config({
     };
   },
   async run() {
+    const isProduction = $app.stage === "production";
+
     const site = new sst.cloudflare.StaticSite("SenchaSite", {
       path: "website",
+      ...(isProduction
+        ? {
+            domain: {
+              name: "senchasound.com",
+              redirects: ["www.senchasound.com"],
+            },
+          }
+        : {}),
+      assets: {
+        fileOptions: [
+          {
+            files: "**",
+            ignore: ["**/*.mp4", "**/*.MP4", "**/*.mov", "**/*.MOV"],
+            cacheControl: "max-age=0,no-cache,no-store,must-revalidate",
+          },
+          {
+            files: "**/*.html",
+            cacheControl: "max-age=0,no-cache,no-store,must-revalidate",
+          },
+          {
+            files: ["**/*.js", "**/*.css"],
+            cacheControl: "max-age=31536000,public,immutable",
+          },
+        ],
+      },
     });
 
     return {
